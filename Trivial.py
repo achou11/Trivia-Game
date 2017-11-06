c1c1b148c112a5bd8e724e21b944da52b7243ba8
haydenc36 <haydenc36@yahoo.com>
2017-11-06 15:33:40 -0600

    yuuKnit T3$tnG
    Unit Testing

--- Trivial.py
+++ Trivial.py
@@ -1,5 +1,34 @@
+# Trivia Game
+# Names:
+
+import unittest
+
+
+def correctAnswer(input, answer):
+    return input == answer
+
+def wrongAnswer(input, answer):
+    return input == answer
+
+
+class MyUnitTest(unittest.TestCase):
+    def test_correct(self):
+        answer = "Taco"
+        input = "Taco"
+        self.assertEqual(correctAnswer(input, answer), True)
+
+    def test_wrong(self):
+        answer = "Taco"
+        input = "burrito"
+        self.assertEqual(wrongAnswer(input, answer), False)
+
+
 
 def main():
+       # Begin unit testing
+       suite = unittest.TestLoader().loadTestsFromTestCase(MyUnitTest)
+       unittest.TextTestRunner(verbosity=2).run(suite)
+
        score = 0
        inFile = open("questionSet.txt", "r")
        ready = eval(input("I want to play a game... \nAre you ready(1/0): "))
@@ -23,4 +52,5 @@ def main():
                      print("score: ", score, "\n")
               
        print("You just finished game... \nFinal score: ", score)
+
 main()
