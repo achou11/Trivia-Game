3c7a67013af582fb467da07b4c4fd36fb77b22de
achou11 <andrew.chou@yahoo.com>
2017-11-08 15:01:45 -0600

    update tests.py

--- tests.py
+++ tests.py
@@ -1,2 +1,12 @@
 import unittest
 import game
+
+
+class FunctionsTest(unittest.TestCase):
+    def test_riddle_list_is_list(self):
+        self.assertIs(type(game.create_riddle_list()), list, msg="riddle_list_create does not return list object")
+
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(FunctionsTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
